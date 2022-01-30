ccfb3ba57d249e7701d34c007d0d9214ddca80f1
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2022-01-30 17:16:17 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
 import Axios from 'axios';
 import './App.css';
-import Header from '.../Components/header/header.js';
-import Footer from '.../Components/footer/footer.js';
 
 function App() {
+  const [firstnameReg, setFirstNameReg] = useState('');
+  const [surnameReg, setSurNameReg] = useState('');
   const [usernameReg, setUsernameReg] = useState('');
   const [passwordReg, setPasswordReg] = useState('');
 
@@ -15,7 +15,7 @@ function App() {
 
 
   const register = () => {
-    Axios.post('http://localhost3001/register', {Username: usernameReg, Password: passwordReg}).then((response) => {
+    Axios.post('http://localhost3001/register', {FirstName: firstnameReg, SurName: surnameReg,  Username: usernameReg, Password: passwordReg}).then((response) => {
         if(response.data.message){
           setLoginStatus(response.data.message);
         }else{
@@ -33,16 +33,26 @@ function App() {
 
   return (
     <>
-      <Header />
       <div className='App'>
         <div className='registration'>
           <h1>Registration</h1>
-          <label className='Heading'>Username:</label>
-          <input type="text" className='username' placeholder='Username' onChange={(e) => { setUsernameReg(e.target.value); } }>
-          </input>
-          <label className='Heading'>Password:</label>
-          <input type="text" className='password' placeholder='password' onChange={(e) => { setPasswordReg(e.target.value); } } />
-          <button className='button1' onClick={register}>Register</button>
+
+          <div className="name-wrap">
+            <label className='Heading'>First name:</label>
+            <input type="text" className='Fname' placeholder='Name e.g. John' onChange={(e) => { setFirstNameReg(e.target.value); } }></input>
+            <label className='Heading'>Surname:</label>
+            <input type="text" className='Sname' placeholder='SurName e.g. Doe' onChange={(e) => { setSurNameReg(e.target.value); } }></input>
+          </div>
+
+          <div className="user-wrap">
+            <label className='Heading'>Username:</label>
+            <input type="text" className='username' placeholder='Username eg. C12345678' onChange={(e) => { setUsernameReg(e.target.value); } }>
+            </input>
+            <label className='Heading'>Password:</label>
+            <input type="text" className='password' placeholder='password e.g. dhVP109..' onChange={(e) => { setPasswordReg(e.target.value); } } />
+            <button className='button1' onClick={register}>Register</button>
+          </div>
+          
         </div>
 
         <div className='login'>
@@ -53,7 +63,6 @@ function App() {
         </div>
           <h1>{loginStatus}</h1>
       </div>
-      <Footer />
     </>
   ); 
 }
