987191ae7e4a25e4a44c8423bc34fe9b21765954
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2022-02-24 16:51:47 +0000

    React render implemented, heaer_index.js-jsx. errors present

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import './header_style.css';
-import './header_index.js'
-function Header(){
+import './header_index.jsx'
+function Header()
+    {
      
-    return(
-        <div className='Headerdiv'>
-            <ul className='Header'>
-                <li className='headerItem'>
-                    <NavLink className="nav-link" to="/homePage">
-                    Home
-                    <span className="sr-only">(current)</span>
-                    </NavLink>
-                </li>
-                <li className="headerItem">
-                    <NavLink className="nav-link" to="/account">
-                    Account
-                    </NavLink>
-                </li>
-                <li className="headerItem">
-                    <NavLink className="nav-link" to="/ModuleReviewForm">
-                    make a review
-                    </NavLink>
-                </li>
-                <li className="headerItem">
-                    <NavLink className="nav-link" to="/logout">
-                    logout
-                    </NavLink>
-                </li>
-                <li className="headerItem">
-                    <NavLink className="nav-link" to="/searchPage">
-                    search page
-                    </NavLink>
-                </li>
+        return(
+            <div className='Headerdiv'>
+                <ul className='Header'>
+                    <li className='headerItem'>
+                        <NavLink className="nav-link" to="/homePage">
+                        Home
+                        <span className="sr-only">(current)</span>
+                        </NavLink>
+                    </li>
+                    <li className="headerItem">
+                        <NavLink className="nav-link" to="/account">
+                        Account
+                        </NavLink>
+                    </li>
+                    <li className="headerItem">
+                        <NavLink className="nav-link" to="/ModuleReviewForm">
+                        make a review
+                        </NavLink>
+                    </li>
+                    <li className="headerItem">
+                        <NavLink className="nav-link" to="/logout">
+                        logout
+                        </NavLink>
+                    </li>
+                    <li className="headerItem">
+                        <NavLink className="nav-link" to="/searchPage">
+                        search page
+                        </NavLink>
+                    </li>
 
-            </ul>
-        </div>
-    )
-}
+                </ul>
+            </div>
+        )
+    }
 
 export default Header;
--- src/Components/header_index.jsx
+++ src/Components/header_index.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import {React} from 'react';
+import  {BrowserRouter as Router, Route, Routes}  from "react-router-dom";
+import {ReactDOM} from 'react-dom';
 import Header from './header.js';
 import  Account from './account.js';
 import  HomePage from './homePage.js';
 import  ModuleReviewForm from './ModuleReviewForm';
 import  Logout from './logout.js';
 import  SearchPage from './searchPage.js';
-   
-  ReactDOM.render(
+  
+ReactDOM.render(
     <Router>
-      <Header />
+      <Header/>
       <Routes>
         <Route path="/homePage" element={<HomePage />} />
         <Route path="/account" element={<Account />} />
@@ -20,7 +19,7 @@ import  SearchPage from './searchPage.js';
         <Route path="/searchpage" element={<SearchPage />}></Route>
       </Routes>
     </Router>
-  );
+);
 
 
   
