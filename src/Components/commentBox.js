90a06248509c73c5d9186c7c9f2c894ef57373f8
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2022-03-18 21:24:44 +0000

    firebase plugins imported to commentbox script

--- src/Components/commentBox.js
+++ src/Components/commentBox.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from './header.js';
 import Footer from './footer.js'
 import './commentBox_index.css';
+import {db} from '../firebase-config';
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
 
 function Comment(){
     return(
