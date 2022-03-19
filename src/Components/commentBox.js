ee5a283c757ecc055e36d5a4c633e6271e6b9560
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2022-03-19 13:38:50 +0000

    post and get methods added to commentbox script

--- src/Components/commentBox.js
+++ src/Components/commentBox.js
@@ -2,16 +2,36 @@ import React from "react";
 import Header from './header.js';
 import Footer from './footer.js'
 import './commentBox_index.css';
+import React, {useState, useEffect} from 'react';
 import {db} from '../firebase-config';
 import { collection, addDoc, getDocs } from "firebase/firestore"; 
 
 function Comment(){
+    const [comment, setComment] = useState("");
+    const commentCollectionRef = collection(db, "review");
+
+    const uploadComment= async () => {
+        await addDoc(commentCollectionRef, {cooment:comment});
+      };
+
+    useEffect(() => {
+        const getComment= async () =>{
+            const data = await getDocs(commentCollectionRef);
+            setComment(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+        };
+        getComment();
+    }, 
+    []); 
+
+
     return(
         <>
         <div classname="comments">
         <h1> enter comment</h1>
-        <textarea className="commentSection" placeholder="enter your comment here"></textarea>
-        <button className='submitButton'> Submit</button>
+        <textarea className="commentSection" placeholder="enter your comment here"  onChange={(event) => {
+                            setComment(event.target.value); } }></textarea>
+
+        <button className='submitButton' onClick={uploadComment}> Submit</button>
         </div>
         </>
     )
