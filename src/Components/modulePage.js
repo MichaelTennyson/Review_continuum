7afb43b17dbc0daf5ede2adc33faa2bd881da17c
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2022-04-02 15:13:40 +0100

    firebase reinitialised

--- src/Components/modulePage.js
+++ src/Components/modulePage.js
@@ -2,20 +2,23 @@ import Header from './header.js';
 import Footer from './footer.js'
 import './review_index.css';
 import { Card, Button } from "react-bootstrap";
-import { useNavigate} from "react-router-dom";
 import React, {useState, useEffect} from 'react';
 import { getDatabase, ref, child, get } from "firebase/database";
+import { render } from '@testing-library/react';
+import { useNavigate} from "react-router-dom";
 
 function ModulePage(){
     const dbRef = ref(getDatabase());
-    get(child(dbRef, `modules/`)).then((snapshot) => {
+    let navigate = useNavigate();
+    get(child(dbRef, `modules`)).then((snapshot) => {
         if (snapshot.exists()) {
-             console.log(snapshot.val());
-             return(
+            render();{
+            return(
                 <>
                 <Header />
-
-                <Card>
+                <h1>Welcome to the module page</h1>
+                <p> on this page, yopu will see the modules that you can currently upload reviews on</p>
+                <Card className="module_card">
                     {snapshot.val().module_name}
                     {snapshot.val().module_code}
                     {snapshot.val().ECTS}
@@ -23,18 +26,21 @@ function ModulePage(){
                     {snapshot.val().module_description}
                     {snapshot.val().module_school}
                     {snapshot.val().module_subject}
+                    <Button className="moduleselection" onClick={navigate("/ModuleReviewForm")}> </Button>
                 </Card>
-
+                <div className="moduleselection"></div>
+        
                 <Footer />
                 </>
             );
+            }  
         } else {
         console.log("No data available");
          }
     }).catch((error) => {
         console.error(error);
     });
-    
+
 }
 
 export default ModulePage;
